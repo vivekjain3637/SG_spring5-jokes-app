3624b703ea88f63e602791823ef18cf95e4bb6a7
vivekjain3637 <61714477+vivekjain3637@users.noreply.github.com>
2022-03-16 14:24:19 +0530

    Setup . Closes #1

---
+++ src/main/java/org/springframework/SG_spring5jokesapp/SgSpring5JokesAppApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.SG_spring5jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgSpring5JokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgSpring5JokesAppApplication.class, args);
+	}
+
+}
